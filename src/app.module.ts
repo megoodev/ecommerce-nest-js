7780c9ae7357af71c292b4bb9b89a470eec934b8
megoodev <mr.hossammostafa2023@gmail.com>
2026-06-29 05:49:22 +0300

    generate auth resourses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, DatabaseModule, ConfigModule.forRoot({
     isGlobal: true,
     
-  })],
+  }), AuthModule],
   controllers: [],
   providers: [],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Delete,
   UseGuards,
   ValidationPipe,
-  ParseIntPipe,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
@@ -19,43 +18,36 @@ import { UserRole } from 'src/utils/enum';
 import { PagenationQueryDto } from './dto/pagenationQueryDto';
 import { UpdateUserDto } from './dto/update.user.dto';
 
+@Roles([UserRole.admin])
+@UseGuards(RolesGuard)
 @Controller('/api/user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('create')
-  @Roles([UserRole.admin])
-  @UseGuards(RolesGuard)
   async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
   }
 
   @Get()
-  @Roles([UserRole.admin])
-  @UseGuards(RolesGuard)
   async findAll(
     @Query(new ValidationPipe({ whitelist: true }))
     query: PagenationQueryDto,
   ) {
     return await this.usersService.findAll(query);
   }
-  @Roles([UserRole.admin])
-  @UseGuards(RolesGuard)
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  @Roles([UserRole.admin])
-  @UseGuards(RolesGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  @Roles([UserRole.admin])
-  @UseGuards(RolesGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
